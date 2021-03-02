26f5ce0552a871812ee33190a37eaeac2812bc04
TedP <ted.pampilon@gmail.com>
2021-03-02 03:09:59 +0000

    head function was created

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr){
+  let first = arr[0];
+  return first;
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([]), "Hello");
