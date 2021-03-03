128c16e1c5a5803b3838c10cbfcfdc13acea7e16
TedP <ted.pampilon@gmail.com>
2021-03-03 02:55:09 +0000

    added without.js

---
+++ without.js
@@ -0,0 +1,29 @@
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      result = false;
+    }
+  }
+  return result;
+};
+
+function assertArraysEqual(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    return console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    return console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+function without(source, itemsToRemove) {
+  let remaining = source;
+  for (let i = 0; i < source.length; i++){
+    for(let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        remaining.splice(i,1);
+      }
+    }
+  }
+  return remaining;
+}
