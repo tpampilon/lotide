a718a57d72394216ddeaf25411e549aed638b5f4
TedP <ted.pampilon@gmail.com>
2021-03-03 02:07:43 +0000

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      result = false;
+    }
+  }
+  return result;
+};
