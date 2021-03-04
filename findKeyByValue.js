6620846bfe9ad36ea0786d375fc10860fde1b527
TedP <ted.pampilon@gmail.com>
2021-03-04 02:40:22 +0000

    added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  // let act = actual.toString();
+  // let exp = expected.toString();
+
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
+  } else {
+    return console.log(`🛑 Assertion Failed: ${actual} !== ${expected} 🛑`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  let ans = undefined;
+  
+  for (key in obj) {
+    if (obj[key] === value) {
+      ans = key
+    } 
+  }
+  return ans;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+// console.log(Object.keys(bestTVShowsByGenre))
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
