1e624aacc531f7c68c9a70b13f8a16f79f302755
TedP <ted.pampilon@gmail.com>
2021-03-08 20:18:45 +0000

    refactored and modularized tail.js

--- tail.js
+++ tail.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  let act = actual.toString();
-  let exp = expected.toString();
-
-  if (act === exp) {
-    return console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
-  } else {
-    return console.log(`🛑 Assertion Failed: ${actual} !== ${expected} 🛑`);
-  }
-};
-
 const tail = function(arr) {
   let last = arr.slice(1);
   return last;
 };
+module.exports = tail;
