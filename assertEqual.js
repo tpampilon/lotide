c94d28f33995db3d12d7036135e42d8dfb278d32
TedP <ted.pampilon@gmail.com>
2021-03-08 19:44:36 +0000

    made assertEqual.js and head.js modularized

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,12 @@
-const assertEqual = function(actual, expected) {
-  let act = actual.toString();
-  let exp = expected.toString();
+const assertEqual = (actual, expected) => {
+  // let act = actual.toString();
+  // let exp = expected.toString();
 
-  if (act === exp) {
+  if (actual == expected) {
     return console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
   } else {
     return console.log(`🛑 Assertion Failed: ${actual} !== ${expected} 🛑`);
   }
 };
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,15 +1,8 @@
-const assertEqual = function(actual, expected) {
-  let act = actual.toString();
-  let exp = expected.toString();
-
-  if (act === exp) {
-    return console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
-  } else {
-    return console.log(`🛑 Assertion Failed: ${actual} !== ${expected} 🛑`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr){
   let first = arr[0];
   return first;
 };
+
+module.exports = head;
